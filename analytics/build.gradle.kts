6bd1e2d2d2311832e3377918501dd324c3843d62
Dmitriy Canishev <canishev.dima2019@gmail.com>
2025-02-09 16:54:07 +0300

    Create publish.yml and Edit gradle analytics module for try upload lib in GithubPages

--- analytics/build.gradle.kts
+++ analytics/build.gradle.kts
@@ -1,7 +1,16 @@
+import java.net.URI
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
+
+var repositoryReleaseUrl: URI = URI.create("https://maven.pkg.github.com/DmitriyCanishev/AnalyticsLibrary")
+
+var libraryGroupId = "com.app.analytics"
+var libraryArtifact = "base"
+var libraryVersion = "0.0.1"
 
 android {
     namespace = "com.app.analytics"
@@ -31,6 +40,64 @@ android {
     }
 }
 
+configurations.api.configure {
+    isCanBeResolved = true
+}
+
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+    api("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+}
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = repositoryReleaseUrl
+
+            credentials {
+                username = System.getenv("PACKAGE_UPLOAD_USER")
+                password = System.getenv("PACKAGE_UPLOAD_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = libraryGroupId
+            artifactId = libraryArtifact
+            version = libraryVersion
+            artifact(
+                "${
+                    File(
+                        buildDir.absolutePath,
+                        "outputs/aar"
+                    )
+                }/${project.name}-release.aar"
+            )
+
+            pom {
+                withXml {
+                    val allDependencies =
+                        project.configurations.api.get().resolvedConfiguration.firstLevelModuleDependencies
+                    val dependenciesNode = asNode().appendNode("dependencies")
+
+                    allDependencies.forEach {
+                        val dependency = dependenciesNode.appendNode("dependency")
+                        dependency.appendNode("groupId", it.moduleGroup)
+                        dependency.appendNode("artifactId", it.moduleName)
+                        dependency.appendNode("version", it.moduleVersion)
+                    }
+                }
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/DmitriyCanishev/AnalyticsLibrary.git"
+                    developerConnection = "scm:git:ssh://github.com/DmitriyCanishev/AnalyticsLibrary.git"
+                    url = "https://github.com/DmitriyCanishev/AnalyticsLibrary"
+                }
+            }
+        }
+    }
 }
